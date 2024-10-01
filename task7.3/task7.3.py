f5e576b56dbf8bb143677e911b309ba17506f5a8
Michael <72989195+MIchael-Dolgov@users.noreply.github.com>
2024-10-01 14:01:50 +0300

    Add files via upload

--- task7.3/task7.3.py
+++ task7.3/task7.3.py
@@ -1,8 +1,11 @@
 def check_rule(string) -> bool:
-    if string.count(str(2)) == 3:
+    if string.count(str(2)) == 3 and issymmetric(string):
         return True
     else:
         return False
+
+def issymmetric(string) -> bool:
+    for i in range()
 
 #  В выборке невозможны симметричные числа
 with open("task7.3.txt", "w") as file:
